7de0ac6dd01eedb06ee139da3c596dfbd28d4140
yangjiayu <1761398181@qq.com>
2020-02-05 20:30:59 +0800

    do some

--- init.el
+++ init.el
@@ -1,2 +1,21 @@
 (tool-bar-mode -1)
 (setq make-backup-files nil)
+(menu-bar-mode -1)
+(global-linum-mode 1)
+
+;;关闭启动动画
+(setq inhibit-splash-screen 1)
+
+;;更改光标样式
+(setq-default cursor-type 'bar)
+
+;;高亮当前行
+(global-hl-line-mode 1)
+
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd"<f2>") 'open-init-file)
+
+(fset 'yes-or-no-p 'y-or-n-p)
